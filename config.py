65520aec61ece342634b129ca3e2c6cb4c1f5299
Lullaby330 <69coder@gmail.com>
2024-02-18 03:32:07 +0700

    Update config.py

--- config.py
+++ config.py
@@ -8,7 +8,7 @@ from logging.handlers import RotatingFileHandler
 load_dotenv("config.env")
 
 # Bot token dari @Botfather
-TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6842997113:AAHU6G1W4zEvHJYhUhdIY4kvAANuhd4CWvU")
+TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "6378558801:AAHGO9KscGurslO1enj8QzR8oVLbH-VZjhU")
 
 # API ID Anda dari my.telegram.org
 APP_ID = int(os.environ.get("APP_ID", "6216349"))
@@ -17,7 +17,7 @@ APP_ID = int(os.environ.get("APP_ID", "6216349"))
 API_HASH = os.environ.get("API_HASH", "5c7418e9f3df6db931caa7354521c55f")
 
 # ID Channel Database
-CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1002055630509"))
+CHANNEL_ID = int(os.environ.get("CHANNEL_ID", "-1001942928633"))
 
 # NAMA OWNER
 OWNER = os.environ.get("OWNER", "1823080600")
@@ -36,8 +36,8 @@ UPSTREAM_BRANCH = os.environ.get("UPSTREAM_BRANCH", "master")
 DB_URI = os.environ.get("DATABASE_URL", "postgres://pwubflfh:cA0OqkeGNei4hjCLulJdGsgKe-CoLT_d@rain.db.elephantsql.com/pwubflfh")
 
 # ID dari Channel Atau Group Untuk Wajib Subscribenya
-FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001767987588"))
-FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "0"))
+FORCE_SUB_CHANNEL = int(os.environ.get("FORCE_SUB_CHANNEL", "-1001979562553"))
+FORCE_SUB_GROUP = int(os.environ.get("FORCE_SUB_GROUP", "1001961327286"))
 
 TG_BOT_WORKERS = int(os.environ.get("TG_BOT_WORKERS", "4"))
 
@@ -47,7 +47,7 @@ START_MSG = os.environ.get(
     "<b>Hello {first}</b>\n\n<b>Saya dapat menyimpan file pribadi di Channel Tertentu dan pengguna lain dapat mengaksesnya dari link khusus.</b>",
 )
 try:
-    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "1823080600").split())]
+    ADMINS = [int(x) for x in (os.environ.get("ADMINS", "5084234580").split())]
 except ValueError:
     raise Exception("Daftar Admin Anda tidak berisi User ID Telegram yang valid.")
 
